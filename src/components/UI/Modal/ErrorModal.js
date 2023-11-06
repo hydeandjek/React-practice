65f41f99edca8c761dc3d7121948c278934ac4d0
hydeandjek <hydeandjek@naver.com>
2023-11-06 12:05:57 +0900

    React potal

--- src/components/UI/Modal/ErrorModal.js
+++ src/components/UI/Modal/ErrorModal.js
@@ -1,23 +1,50 @@
 import React from 'react';
+
+// portal을 사용하기 위한 import
+import ReactDOM from 'react-dom';
 import Card from '../Card';
 import Button from '../Button/Button';
 import styles from './ErrorModal.module.css';
 
+const Backdrop = ({ onConfirm }) => {
+  return (
+    <div>
+      className = {styles.Backdrop}
+      onClick={onConfirm}
+    </div>
+  );
+};
+
+const ModalOverlay = ({ title, message, onConfirm }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <>
-      <div className={styles.backdrop} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          message={message}
+          onConfirm={onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
     </>
   );
 };
